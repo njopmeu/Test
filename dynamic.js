9ba679cde69a5f817e2d4e4c06f6b6959713e4a3
njopmeu <160629228+njopmeu@users.noreply.github.com>
2024-02-22 13:34:50 +0100

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -2,9 +2,5 @@ export default {
   key: "jwhef8674379u4zut894utgughu894u",
   Keycloak_Url: "https:keycloak.com",
   client_id: "git",
-
-  async set_key: (newKey) => {
-  jsObjectName.key = newKey;
-}
   
 }
