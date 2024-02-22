c5a3db58bc0d6800addd89f358206705ef801ef0
njopmeu <160629228+njopmeu@users.noreply.github.com>
2024-02-22 13:41:36 +0100

    Delete av.js

--- av.js
+++
@@ -1,8 +0,0 @@
-export default {
-  colorValue: "#4A90E2", // The color value is set when the JSObject is defined
-};
-
-// Example of re-assigning the variable
-setColorValue: (newColor) => {
-  jsObjectName.colorValue = newColor;
-};
